d11f56d1aea09abb1863901813d47b000604633e
Aarav Sane <aaravtennis@gmail.com>
2022-01-16 09:27:05 -0800

    Update customer.hpp
    coding style changes

--- customer.hpp
+++ customer.hpp
@@ -1,4 +1,3 @@
-
 #ifndef customer_hpp
 #define customer_hpp
 #include <vector>
@@ -15,32 +14,24 @@ class customer{
   
   private:
   //what is included in a customer
-      vector<string> nameStr;
-     // order* custOrder;
-      string name="";
-      
-      
+        vector<string> nameStr;
+        // order* custOrder;
+        string name;
+        //an order pointer points at an order
   public:
-  
       customer(){
-        //random generation of names
-        nameStr={"Rose","Rosalie", "Ro", "Rosario", "Roman", "Rosa", "Rowan", "Robert", "Ronald", "Roald", "Rod"};
-        int rando=rand() %nameStr.size();
-        name= nameStr.at(rando);
+        nameStr ={"Rose", "Rosalie", "Ro", "Rosario", "Roman", "Rosa", "Rowan", "Robert", "Ronald", "Roald", "Rod"};
+        int rando = rand() % nameStr.size();
+        name = nameStr.at(rando);
         //setName(name);
-        cout<<"Here comes a customer!"<<endl<<endl;
+        cout << "Here comes a customer!" << endl <<endl;
         string userAns;
-        cin>>userAns;
-        cout<<endl;
-        cout<<"Hello my name is "<<getName()<<endl;
+        cin >> userAns;
+        cout << endl;
+        cout << "Hello my name is " << getName() << endl;
       }
         string getName() const {return name;}
         //string setName(string name) const {this->name;}
-
-        
-        
-
-
 };
 
 #endif
